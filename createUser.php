ad26d4e5595516e3ee0d5f65bd584283b1bd6bb0
haraldvinje <haraldvinje@gmail.com>
2017-03-14 12:14:56 +0100

    created function in createUser.php

--- createUser.php
+++ createUser.php
@@ -1,5 +1,6 @@
 <html>
 <?php
+
 $servername = "mysql.stud.ntnu.no";
 $username = "capybotpu";
 $password = "1234";
@@ -15,8 +16,21 @@ if (!$con) {
 }
 
 
-$sql = "INSERT INTO `User` (`ID`, `name`, `e_mail`, `is_student`) 
-VALUES (NULL, 'new guy', 'newguy@cooldomain.com', '1')";
+function addUser($name, $email, $is_student){
+	$con = dbConnect();
+	$sql = "INSERT INTO 'User' ('ID', 'name', 'e_mail', 'is_student') 
+	VALUES (NULL,". $name.",". $email.",". $is_student.")";
+	if (mysqli_query($con, $sql)){
+		echo "New guy successfully added";
+	} 	
+	else{
+		echo "Failed to insert new guy";
+	}
+}
+
+
+$sql = "INSERT INTO 'User' ('ID', 'name', 'e_mail', 'is_student') 
+VALUES (NULL, 'new guys', 'newguy@cooldomains.com', '1')";
 if (mysqli_query($con, $sql)){
 	echo "New guy successfully added";
 } else{
