b5ca89c694b4c8cd32a9e838dcc4339786eb10ec
Kristiane Westgård <kristianeaw@gmail.com>
2017-03-21 15:44:51 +0100

    more ajax

--- Lecturer/lecturer.js
+++ Lecturer/lecturer.js
@@ -1,11 +1,10 @@
-function testFunction() {
-    console.log("klikk");
+function createLecture() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("hei").innerHTML = this.responseText;
+            document.getElementById("main").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "test.txt", true);
+    xhttp.open("POST", "createLecture.txt", true);
     xhttp.send();
 }
