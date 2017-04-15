9f81e3a5c5ef3cce563863d22f6b9178c3e4a02d
Kristiane Westgård <kristianeaw@gmail.com>
2017-04-15 14:10:54 +0200

    created sign in and create user forms

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
+function signIn() {
 
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-};
+
+}
+
+function createNewUser() {
+    var createUser = document.getElementById("createUser");
+    var logIn = document.getElementById("logIn");
+
+    createUser.style.display = "block";
+    logIn.style.display = "none";
+
+}
+
+function signInClick() {
+    console.log("Lorem ipsum dolor sit amet");
+    var createUser = document.getElementById("createUser");
+    var logIn = document.getElementById("logIn");
+
+    logIn.style.display = "block";
+    createUser.style.display = "none";
+}
