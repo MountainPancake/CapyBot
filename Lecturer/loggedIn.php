836898e20a41d7bf74a31566f729692dd7ef3813
Kristiane Westgård <kristianeaw@gmail.com>
2017-04-15 10:53:37 +0200

    vet ik

--- Lecturer/loggedIn.php
+++ Lecturer/loggedIn.php
@@ -32,7 +32,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">capybot</a>
+                <a class="navbar-brand" onclick="overview()">capybot</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
