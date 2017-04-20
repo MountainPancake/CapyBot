6cf19295226ae8244683d4505d2d127443cce5ba
Fride Skarseth <frideskarz28@gmail.com>
2017-04-20 17:45:48 +0200

    Update insertPost.php

--- Student/insertPost.php
+++ Student/insertPost.php
@@ -2,7 +2,7 @@
     session_start();
     require '../Database.php';
     $lecture_ID = $_SESSION["lecturePin"];
-    $text = $_POST["question"];text;
+    $text = $_POST["question"];
     $db = new Database();
     $db->insertPost($lecture_ID,$text);
 ?>
