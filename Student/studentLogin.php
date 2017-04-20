b6073dfeb1aa1954ebe82ed84bd28a346f87166b
haraldvinje <haraldvinje@gmail.com>
2017-04-20 17:47:23 +0200

    now possible to create a new student user and login with this

--- Student/studentLogin.php
+++ Student/studentLogin.php
@@ -2,31 +2,30 @@
 
 session_start();
 
-require 'Database.php';
+include '../Database.php';
 
 $email = $_POST["email"];
 $password = $_POST["password"];
 
 
-echo "Hello";
-/*
+
+
 $db = new Database();
 
-/*
+
 $success = $db->getUserByEmailAndPassword($email, $password);
 
-echo $success;
 
-echo $success["is_student"];
+
 
 if ($success and $success["is_student"]=="1"){
 	$_SESSION["email"] = $email;
 	$_SESSION["password"] = $password;
-	echo "Success";
+	echo "success";
 }
 else{
-	echo "Invalid";
+	echo "invalid";
 }
-*/
+
 
 ?>
