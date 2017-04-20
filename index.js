a99f798fc1c9388f41aa78b5faf97280f98f8571
haraldvinje <haraldvinje@gmail.com>
2017-04-20 17:49:39 +0200

    index.html and index.js updated to make create user possible

--- index.js
+++ index.js
@@ -10,13 +10,38 @@ function studentLogin(){
         url: "Student/studentLogin.php",
         data: dataString,
         success: function(text) {
-            /*if(text == "success"){
-                location.href = "student_notLoggedIn.php"
+            if(text == "success"){
+                location.href = "Student/studentNotInLecture.html"
             }
             else{
                 alert("Login failed. \nUsername and/or password incorrect.")
-            }*/
-            console.log(text);
+            }
+        }, 
+        error: function (jqXHR, exception) {
+            console.log(jqXHR);
+        }
+    });
+}
+
+//Haralds kode for å lage studentbruker
+
+function createStudentUser(){
+	event.preventDefault();
+    var dataString = "email="+document.getElementById("newEmail").value+"&password="+document.getElementById("newPassword").value;
+    console.log(dataString);
+    $.ajax({
+        type: "POST",
+        url: "createStudentUser.php",
+        data: dataString,
+        success: function(text) {
+        	console.log(text)
+            if(text == "success"){
+            	console.log("inni success bro")
+                location.href = "index.html"   
+            }
+            else{
+                alert("Failed to create user. \nThe username may already exist.")
+            }
         }, 
         error: function (jqXHR, exception) {
             console.log(jqXHR);
