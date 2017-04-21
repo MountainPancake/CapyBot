0066798198ff94f516e76c11fdedc947952f7a7f
frideska <frideskarz28@gmail.com>
2017-04-21 13:04:51 +0200

    Fungerende upvoting

--- Student/studentUpvote.php
+++ Student/studentUpvote.php
@@ -1,7 +1,9 @@
 <?php
     session_start();
     require '../Database.php';
-    $post_ID = $_POST["ID"];
+    $post_ID = $_GET["ID"];
     $db = new Database();
-    $db->upvotePostByID($ID);
+    $db->upvotePostByID($post_ID);
+    $post = $db->getPostByID($post_ID);
+    echo json_encode($post);
 ?>
