145994707305c2676afe63a3b4cae7f40d02545f
frideska <frideskarz28@gmail.com>
2017-04-23 12:52:40 +0200

    getLecture.php oppdatert

--- Student/getLecture.php
+++ Student/getLecture.php
@@ -1,14 +1,9 @@
 <?php
     session_start();
-    require '../Database.php';
+    include '../Database.php';
+    $lecture_ID = $_SESSION["lecturePin"];
     $db = new Database();
-    $dbLecture = $db->getLectureByID($_SESSION["lecturePin"]);
-    $lecture->PIN = $dbLecture["ID"];
-    $lecture->date = $dbLecture["date"];
-    $lecture->responses = ["Slow down","Speed up", "Too hard","Too easy"];
-    $lecture->category = $dbLecture["category"];
-
-    $jsonLecture = json_encode($lecture);
-
-    echo $jsonLecture;
+    $success = $db->getResponseTypesByLectureID($lecture_ID);
+    $data = json_encode($success);
+    echo $data;
 ?>
