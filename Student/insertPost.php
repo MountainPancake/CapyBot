7bce748f51643d063a8194c9c9b81f5f09d5d088
Zawadi Svela <z.b.svela@gmail.com>
2017-04-24 15:48:04 +0200

    Bruker hentes fra databasen før post settes inn

--- Student/insertPost.php
+++ Student/insertPost.php
@@ -2,8 +2,12 @@
     session_start();
     require '../Database.php';
     $lecture_ID = $_SESSION["lecturePin"];
-    $posted_by_ID = $_SESSION["email"];
+    $posted_by_email = $_SESSION["email"];
     $text = $_POST["question"];
     $db = new Database();
-    $db->insertPost($lecture_ID,$posted_by_ID,$text);
+    $user = $db->getUserByEmail($posted_by_email);
+    if($user){
+        $posted_by_ID = $user["ID"];
+        $db->insertPost($lecture_ID,$posted_by_ID,$text);
+    }
 ?>
