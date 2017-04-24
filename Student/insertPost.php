3645c58ff8523dea48553a191f4aab0934589d2c
Zawadi Svela <z.b.svela@gmail.com>
2017-04-24 15:31:40 +0200

    Poster har nå emailen til de som skrev den

--- Student/insertPost.php
+++ Student/insertPost.php
@@ -2,7 +2,8 @@
     session_start();
     require '../Database.php';
     $lecture_ID = $_SESSION["lecturePin"];
+    $posted_by_ID = $_SESSION["email"];
     $text = $_POST["question"];
     $db = new Database();
-    $db->insertPost($lecture_ID,$text);
+    $db->insertPost($lecture_ID,$posted_by_ID,$text);
 ?>
